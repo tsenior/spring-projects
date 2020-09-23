4413338a4f6f9b8d0b641498d4c7f498a6d51781
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-09-23 14:06:05 +0200

    add files

---
+++ src/main/java/com/tinyiko/learn/code/demo/LearnSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.tinyiko.learn.code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringAppApplication.class, args);
+    }
+
+}
