5d61ab195252794480192fe5b6429b933e6d4fa0
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-09-24 10:05:33 +0200

    added embedded controller

--- src/main/java/com/tinyiko/learn/code/demo/LearnSpringAppApplication.java
+++ src/main/java/com/tinyiko/learn/code/demo/LearnSpringAppApplication.java
@@ -1,13 +1,32 @@
 package com.tinyiko.learn.code.demo;
 
+import com.tinyiko.learn.code.demo.data.entity.Room;
+import com.tinyiko.learn.code.demo.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearnSpringAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnSpringAppApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/rooms")
+    public class RoomController{
+        @Autowired
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        public Iterable<Room> getRooms(){
+            return this.roomRepository.findAll();
+        }
+
     }
 
 }
