9d89aeb52ed54cc761a8bf8ebc94196bfe60892b
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-09-26 11:30:54 +0200

    added logic to find a Reservation By a Reservation Date

--- src/main/java/com/tinyiko/learn/code/demo/business/service/ReservationService.java
+++ src/main/java/com/tinyiko/learn/code/demo/business/service/ReservationService.java
@@ -1,6 +1,9 @@
 package com.tinyiko.learn.code.demo.business.service;
 
 import com.tinyiko.learn.code.demo.business.domain.RoomReservationDTO;
+import com.tinyiko.learn.code.demo.data.entity.Guest;
+import com.tinyiko.learn.code.demo.data.entity.Reservation;
+import com.tinyiko.learn.code.demo.data.entity.Room;
 import com.tinyiko.learn.code.demo.data.repository.GuestRepository;
 import com.tinyiko.learn.code.demo.data.repository.ReservationRepository;
 import com.tinyiko.learn.code.demo.data.repository.RoomRepository;
@@ -8,7 +11,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +25,28 @@ public class ReservationService {
 
 
     public List<RoomReservationDTO> getRoomReservationForDate(Date date){
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservationDTO> roomReservationDTOMap = new HashMap<>();
+
+        rooms.forEach(room -> {
+            RoomReservationDTO roomReservationDTO = new RoomReservationDTO();
+            roomReservationDTO.setRoomId(room.getRoomId());
+            roomReservationDTO.setRoomName(room.getRoomName());
+            roomReservationDTO.setRoomNumber(room.getRoomNumber());
+
+            roomReservationDTOMap.put(room.getRoomId(), roomReservationDTO);
+        });
+
+        Iterable<Reservation> reservations = reservationRepository.findReservationByReservationDate(new java.sql.Date(date.getTime()));
+        reservations.forEach(reservation -> {
+            RoomReservationDTO roomReservationDTO = roomReservationDTOMap.get(reservation.getRoomId());
+            roomReservationDTO.setDate(date);
+            Guest guest = this.guestRepository.findById(reservation.getGuestId()).get();
+            roomReservationDTO.setFirstName(guest.getFirstName());
+            roomReservationDTO.setLastName(guest.getLastName());
+            roomReservationDTO.setGuestId(guest.getGuestId());
+        });
+
         return null;
     }
 }
