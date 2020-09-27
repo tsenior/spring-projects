eee454f546bdefa905407a989daed8ef4fbf474d
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-09-27 05:40:59 +0200

    added findReservationByReservationDate method query

--- src/main/java/com/tinyiko/learn/code/demo/data/repository/ReservationRepository.java
+++ src/main/java/com/tinyiko/learn/code/demo/data/repository/ReservationRepository.java
@@ -3,7 +3,10 @@ package com.tinyiko.learn.code.demo.data.repository;
 import com.tinyiko.learn.code.demo.data.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Date;
+
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 
 
 }
