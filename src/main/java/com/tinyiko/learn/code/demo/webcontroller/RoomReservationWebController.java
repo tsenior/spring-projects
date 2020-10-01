74e3606d312aca69459535b85e5f238630ccf78a
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-10-01 16:39:42 +0200

    implement controller to get reservations

--- src/main/java/com/tinyiko/learn/code/demo/webcontroller/RoomReservationWebController.java
+++ src/main/java/com/tinyiko/learn/code/demo/webcontroller/RoomReservationWebController.java
@@ -1,7 +1,39 @@
 package com.tinyiko.learn.code.demo.webcontroller;
 
-public class RoomReservationWebController {
+import com.tinyiko.learn.code.demo.business.domain.RoomReservationDTO;
+import com.tinyiko.learn.code.demo.business.service.ReservationService;
+import com.tinyiko.learn.code.demo.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationWebController {
+//we first need a way to parse a date to a method - see DateUtils class
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public RoomReservationWebController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public String getReservations(@RequestParam(value = "date", required = false) String dateString, Model model){
+        Date date = DateUtils.createDateFromDateSting(dateString);
+        List<RoomReservationDTO> roomReservations = this.reservationService.getRoomReservationForDate(date);
+        model.addAttribute("roomReservations",roomReservations);
+        return "reservations";
+
+    }
 
     /*
     *
