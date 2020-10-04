3cc5db2527c0b5c016fcd1c2802f70557acf5deb
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-10-04 05:58:05 +0200

    added getReservations - mocked roomReservations data

--- src/test/java/com/tinyiko/learn/code/demo/webcontrollertest/RoomReservationWebControllerTest.java
+++ src/test/java/com/tinyiko/learn/code/demo/webcontrollertest/RoomReservationWebControllerTest.java
@@ -1,13 +1,20 @@
 package com.tinyiko.learn.code.demo.webcontrollertest;
 
+import com.tinyiko.learn.code.demo.business.domain.RoomReservationDTO;
 import com.tinyiko.learn.code.demo.business.service.ReservationService;
+import com.tinyiko.learn.code.demo.utils.DateUtils;
 import com.tinyiko.learn.code.demo.webcontroller.RoomReservationWebController;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(RoomReservationWebController.class)
@@ -18,4 +25,20 @@ public class RoomReservationWebControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    public void getReservations() throws Exception{
+        String dateString = "220-01-01";
+        Date date = DateUtils.createDateFromDateSting(dateString);
+        List<RoomReservationDTO> roomReservations = new ArrayList<>();
+        RoomReservationDTO roomReservation = new RoomReservationDTO();
+        roomReservation.setLastName("Tinyiko");
+        roomReservation.setFirstName("Chauke");
+        roomReservation.setDate(date);
+        roomReservation.setGuestId(1);
+        roomReservation.setRoomId(100);
+        roomReservation.setRoomName("Room C");
+        roomReservation.setRoomNumber("c1");
+        roomReservations.add(roomReservation);
+    }
 }
